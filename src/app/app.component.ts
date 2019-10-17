45cfc4d790bec6b319e63c51249309f52386cc25
ArielPrez <arielpgmm@gmail.com>
2019-10-16 23:56:13 -0400

    second test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-crncy-man',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Currency-Manager';
+  ready = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.ready = true;
+    }, 3500);
+  }
+  showExample() {
+    alert('You should see now the Angular default example.');
+  }
 }
