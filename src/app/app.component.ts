e3480e95b6c35536a8cf6ec60dd3a566bc23ffb2
ArielPrez <arielpgmm@gmail.com>
2019-10-20 13:08:34 -0400

    Angular example show up with a button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Currency-Manager';
   ready = false;
+  angularActive = false;
   currencies: any = [
-      {active: true, name: 'Dollar'},
-      {active: true, name: 'Euro'},
-      {active: true, name: 'Pound sterling'},
-      {active: false, name: 'Bitcoin'}
+      {active: true, name: 'Dollar', rate: 1},
+      {active: true, name: 'Euro', rate: 0.8973438622},
+      {active: true, name: 'Pound sterling', rate: 0.7756191673},
+      {active: true, name: 'Yen', rate: 108.5516870065},
+      {active: false, name: 'Bitcoin', rate: 8059.99}
   ];
   lat = 25.7799355;
   lng = -80.1866916;
-
   constructor() {
     setTimeout(() => {
       this.ready = true;
@@ -24,5 +25,7 @@ export class AppComponent {
   }
   showExample() {
     alert('You should see now the Angular default example.');
+    this.angularActive = true;
+    this.ready = false;
   }
 }
